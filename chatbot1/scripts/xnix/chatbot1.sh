b29f4cbdad69d415f2758a50a8a7a6aff61484f8
Keith Sterling <keiffster@gmail.com>
2019-05-10 09:22:59 +0100

    Initial git commit for open chatbot apiwork

---
+++ chatbot1/scripts/xnix/chatbot1.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+clear
+
+
+python3 -m programy.clients.restful.flask.client --config ../../config/xnix/chatbot1-flask.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
+
---
+++ chatbot2/scripts/xnix/chatbot2.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+clear
+
+
+python3 -m programy.clients.restful.flask.client --config ../../config/xnix/chatbot2-flask.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
+

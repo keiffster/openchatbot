8c6578e8604d7b4784d2bdf6a66ebf1fa7ecc171
Keith Sterling <keiffster@gmail.com>
2019-05-22 06:04:02 +0100

    Updates to webchat config to work with metabot

--- program-y/scripts/xnix/program-y-webchat.sh
+++ program-y/scripts/xnix/program-y-webchat.sh
@@ -4,5 +4,5 @@ clear
 
 export PYTHONPATH=../../src:$PYTHONPATH
 
-python3 -m templatey.clients.restful.flask.webchat.client --config ../../config/xnix/config.webchat.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
+python3 -m programy.clients.restful.flask.webchat.client --config ../../config/xnix/config.webchat.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
 
